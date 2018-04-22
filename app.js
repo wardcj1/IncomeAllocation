315af8334a63a9bb3c647f1b5d59c0ca4fe99985
chris ward <ward.christopherj@gmail.com>
2018-04-21 23:06:20 -0700

    .

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-  
+$(document).ready(function(){
+	$("#incomeTable").hide();
+	$("#lumpSumTable").hide();
+
+    $("#btn1").click(function(){
+        $("#test1").text("Hello world!");
+    });
+    
+    $("#btn2").click(function(){
+        $("#test2").html("<b>Hello world!</b>");
+    });
+    
+    $("#btn3").click(function(){
+        $("#test3").val("Dolly Duck");
+    });
+
+    $("#submitBtn").click(function(){
+        $("#incomeTable").slideToggle();
+    });
+
+    $("#proceedBtn").click(function(){
+        $("#lumpSumTable").slideToggle();
+    });
 });
