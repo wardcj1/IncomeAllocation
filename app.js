8e50645d1e9b710cd72fda55c6a3e8d3d30449ba
chris ward <ward.christopherj@gmail.com>
2018-04-22 22:08:35 -0700

    .

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-$(document).ready(function(){
-	$("#incomeTable").hide();
-	$("#budget").hide();
-	$("#lumpSumTable").hide();
-
     // $("#btn1").click(function(){
     //     $("#test1").text("Hello world!");
     // });
@@ -14,6 +9,11 @@ $(document).ready(function(){
     // $("#btn3").click(function(){
     //     $("#test3").val("Dolly Duck");
     // });
+
+$(document).ready(function(){
+	$("#incomeTable").hide();
+	$("#budget").hide();
+	$("#lumpSumTable").hide();
 
     $("#submitBtn").click(function(){
         $("#incomeTable").slideToggle();
@@ -27,3 +27,13 @@ $(document).ready(function(){
         $("#lumpSumTable").slideToggle();
     });
 });
+
+function displayVals() {
+  var singleValues = $( "#single" ).val();
+  var multipleValues = $( "#multiple" ).val() || [];
+  $( "#t1A3" ).html( "<b>Single:</b> " + "<br>" + singleValues + "<br>" +
+    " <b>Multiple:</b> " + "<br>" + multipleValues.join( ", " ) );
+}
+ 
+$( "select" ).change( displayVals );
+displayVals();
